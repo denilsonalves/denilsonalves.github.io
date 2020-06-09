c81d96987c76f35f8cfba985998887bef13c14b3
denilsonalves <denilson.souza@gmail.com>
2020-06-08 22:52:53 -0300

    novo site pessoal

---
+++ js/activeLink.js
@@ -0,0 +1,9 @@
+function linkAtivo(tag) {
+  const listaMenu = document.getElementById('lista-menu');
+  let linkItem = listaMenu.getElementsByTagName('a');
+
+  for (let i = 0; i < linkItem.length; i++) {
+    linkItem[i].classList.remove('active');
+  }
+  tag.classList.add('active');
+}
